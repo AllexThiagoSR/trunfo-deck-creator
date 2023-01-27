ee5b49c40dc785edff5948aa6ab713a9c0c6dc1d
Allex Thiago <allexthiagogatinho502@gmail.com>
2023-01-27 14:48:45 -0300

    Requisito 1: Cria o componente Form

---
+++ src/components/Form.js
@@ -0,0 +1,10 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+class Form extends React.Component {
+
+}
+
+Form.propTypes = {};
+
+export default Form;
---
+++ src/components/Input.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+class Input extends React.Component {
+  render() {
+    return (
+      <label>
+        <input />
+      </label>
+    );
+  }
+}
+
+Input.propTypes = {};
+
+export default Input;

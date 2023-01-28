e8a84df7e6bc651feef4c48cd3bb54fe776082ad
Allex Thiago <allexthiagogatinho502@gmail.com>
2023-01-27 22:24:57 -0300

    Requisito 5: Concluido

--- src/App.js
+++ src/App.js
@@ -7,19 +7,51 @@ class App extends React.Component {
     cardName: '',
     cardDescription: '',
     cardImage: '',
-    cardRare: '',
+    cardRare: 'normal',
     cardTrunfo: false,
     hasTrunfo: false,
-    cardAttr1: '',
-    cardAttr2: '',
-    cardAttr3: '',
-    isSaveButtonDisabled: false,
+    cardAttr1: '0',
+    cardAttr2: '0',
+    cardAttr3: '0',
+    isSaveButtonDisabled: true,
+  };
+
+  checkMissingInfos = (...infos) => infos.some((value) => value === '');
+
+  sum = (...nums) => nums.reduce((acc, curr) => acc + parseInt(curr, 10), 0);
+
+  checkAtributsValues = (maxSum, ...attrs) => this.sum(...attrs) > maxSum;
+
+  checkLimitAtributs = (limit, ...attrs) => attrs
+    .some((attr) => parseInt(attr, 10) > limit || parseInt(attr, 10) < 0);
+
+  checkDisableButtonConditions = () => {
+    const maxSum = 210;
+    const limit = 90;
+    const { cardName, cardImage, cardDescription, cardRare,
+      cardAttr1, cardAttr2, cardAttr3 } = this.state;
+    const missingAnyInfo = this.checkMissingInfos(
+      cardName,
+      cardImage,
+      cardDescription,
+      cardRare,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+    );
+    const attrsValidValues = this
+      .checkAtributsValues(maxSum, cardAttr1, cardAttr2, cardAttr3);
+    const attrsLimitValues = this
+      .checkLimitAtributs(limit, cardAttr1, cardAttr2, cardAttr3);
+    this.setState({
+      isSaveButtonDisabled: (missingAnyInfo || attrsValidValues || attrsLimitValues),
+    });
   };
 
   handleChange = ({ target: { name, type, checked, value } }) => {
     this.setState({
       [name]: (type === 'checkbox' ? checked : value),
-    });
+    }, this.checkDisableButtonConditions);
   };
 
   handleClick = (event) => {
